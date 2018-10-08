5ec1e9b62fe9eb28820ecfdaf13068de7f6ed6db
Riccardo Crippa <therickys93@gmail.com>
2018-10-08 18:53:24 +0200

    Initial commit

---
+++ Example/WikiSDK/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  WikiSDK
+//
+//  Created by therickys93 on 10/08/2018.
+//  Copyright (c) 2018 therickys93. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
