df4be91aaf4668975c0565acd9e1255c67fc851f
Riccardo Crippa <therickys93@gmail.com>
2019-01-30 18:42:07 +0100

    aggiunto test led

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -5,24 +5,17 @@ class Tests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
-        }
+    func testLed() {
+        let led = Led(name: "name", key: "key", position: 1)
+        XCTAssertEqual("name", led.name)
+        XCTAssertEqual("key", led.key)
+        XCTAssertEqual(2, led.position)
     }
     
 }
