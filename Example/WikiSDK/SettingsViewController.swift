0ea551ebebb54f31017fb5f49af2e33a22d01faf
Riccardo Crippa <therickys93@gmail.com>
2019-06-29 11:04:51 +0200

    prima implementazione settings

--- Example/WikiSDK/SettingsViewController.swift
+++ Example/WikiSDK/SettingsViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class SettingsViewController: UITableViewController {
 
+    @IBOutlet weak var appIDTextField: UITextField!
+    @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var classNameTextField: UITextField!
+    @IBOutlet weak var fieldTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         self.title = Wiki.Controllers.Settings.TITLE
+        self.appIDTextField.text = Utils.loadParseAppId()
+        self.urlTextField.text = Utils.loadParseURL()
+        self.classNameTextField.text = Utils.loadParseClassName()
+        self.fieldTextField.text = Utils.loadParseField()
     }
     
 }
